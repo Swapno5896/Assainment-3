4ad5c70c9eb1bddec4deb358fc3721e3ea8e5e4e
swapno <Swapno5896@yahoo.com>
2021-01-21 22:47:48 +0600

    one finish

---
+++ assignment.js
@@ -0,0 +1,15 @@
+//https://github.com/Swapno5896/Assainment-3
+
+function kilometerToMeter(kiloMeter){
+    if(kiloMeter<=0){
+        console.log("you can't enter 0 or negetive value");
+    }
+    else{
+        return kiloMeter * 1000
+
+    }
+}
+var a =kilometerToMeter(-4)
+console.log(a)
+
+
